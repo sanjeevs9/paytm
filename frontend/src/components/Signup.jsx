a5a6c55ebf3130ad8214484fb2427877a5fa4f9a
sanjeevs9 <sanjeev.19kr@gmail.com>
2024-01-24 19:52:11 +0530

    signup Component completed

--- frontend/src/components/Signup.jsx
+++ frontend/src/components/Signup.jsx
@@ -1,37 +1,40 @@
 export function Signup(){
     return(
-        <div class="flex flex-col  bg-black text-white rounded-lg w-80">
-<div class="flex flex-col p-5 items-center ">
-  <div class="text-2xl font-bold pb-2 ">
-    <h1>Sign up</h1>
-  </div>
-  
-  <div>Enter your information to create an</div>
-  <div>account</div>
-  </div>
-
-  <div class="p-5" >
-    
-  <div class="pb-2">First Name</div>
-  <input class="rounded-sm "></input>
-  <div class="pb-3 ">Last Name</div>
-   <input ></input>
-  <div class="pb-3">Email</div>
-   <input class=""></input>
-   <div class="pb-3">Passsword</div>
-   <div class="pb-3"><input ></input></div>
-    <div class="pb-3">
-    <button class=" bg-gray-500 p-1 w-full rounded ">Sign up</button>
-    </div>
-     <div class="flex flex-row gap-2 text-sm justify-center">
-    <div>Already have an account?</div>
-    <div class="underline">Login</div>
-    </div>
-    </div>
-
-   
-
-  
-</div>
+      <div className="flex flex-col  bg-white rounded-lg w-80">
+      <div class="flex flex-col p-5 items-center ">
+        <div className=""></div>
+        <div className="text-2xl font-bold pb-2 text-[#18181b]">
+          <h1>Sign up</h1>
+        </div>
+        
+        <div className="">Enter your information to create an</div>
+        <div>account</div>
+        </div>
+      
+        <div className="p-5" >
+          
+        <div className="pb-2 text-black font-medium">First Name</div>
+        <input className="rounded-md bg-transparent w-full h-full outline outline-0 focus:outline-0 border border-black p-1"></input>
+        <div className="pb-3 text-black font-medium">Last Name</div>
+         <input className="rounded-md bg-transparent w-full h-full outline outline-0 focus:outline-0 border border-black p-1"></input>
+        <div className="pb-3 text-black font-medium">Email</div>
+         <input className="rounded-md bg-transparent w-full h-full outline outline-0 focus:outline-0 border border-black p-1"></input>
+         <div className="pb-3 text-black font-medium">Passsword</div>
+         <div className="pb-3 "><input className="rounded-md bg-transparent w-full h-full outline outline-0 focus:outline-0 border border-black p-1" ></input></div>
+          <div className="pb-3">
+          <button className=" bg-[#18181b] text-white p-1 w-full rounded ">Sign up</button>
+          </div>
+           <div className="flex flex-row gap-2 text-sm justify-center">
+          <div>Already have an account?</div>
+          <div className="underline">Login</div>
+          
+        
+          </div>
+          </div>
+      
+         
+      
+        
+      </div>
     )
 }
