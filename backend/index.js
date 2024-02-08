1ccc1523ee60ce7b3e44c48979e3439413d706d3
sanjeevs9 <sanjeev.19kr@gmail.com>
2024-02-08 22:36:33 +0530

    some corrections

--- backend/index.js
+++ backend/index.js
@@ -9,11 +9,13 @@ const mainRouter = require('./routes/index');
 
 
 const PORT = 3000; 
-const IP_ADDRESS = '0.0.0.0'; // Bind to all available network interfaces
 
-// Bind the server to the specified IP address and port
-app.listen(PORT, IP_ADDRESS, () => {
-  console.log(`Server is running on ${IP_ADDRESS}:${PORT}`);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
 
+app.get('/', (req, res) => {
+  res.send('bad request');
+})
 app.use('/api/v1', mainRouter);
